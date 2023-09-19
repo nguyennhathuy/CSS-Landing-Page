347f54aa415aa52a16fa8e414ff5809aa998d144
Huy Nguyen <nhathuy120796@gmail.com>
2023-09-19 21:31:27 +0700

    js file tuesday update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert('log to javascript')
+const bookList = [
+    {
+        name: "Refactoring",
+        author: "Martin Fowler",
+        topic: "Programming"
+    },
+    {
+        name: "Designing Data-Intensive Applications",
+        author: "Martin Kleppmann",
+        topic: "Database"
+    },
+    {
+        name: "The Phoenix Project",
+        author: "Gene Kim",
+        topic: "DevOps"
+    }
+]
